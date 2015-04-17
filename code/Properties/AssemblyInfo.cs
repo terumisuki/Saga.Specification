977169ae7967289921d43e5480d06fed934b3df4
terumisuki <terumisuki@users.noreply.github.com>
2015-04-17 18:43:05 -0400

    Making project compilable "as is"

--- code/Properties/AssemblyInfo.cs
+++ code/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Interfaces")]
+[assembly: AssemblyTitle("Saga.Specification")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Interfaces")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCompany("Alex Wooten")]
+[assembly: AssemblyProduct("Saga.Specification")]
+[assembly: AssemblyCopyright("Copyright © Alex Wooten 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
